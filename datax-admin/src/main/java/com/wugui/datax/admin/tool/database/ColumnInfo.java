458424fb01830b9d816cd676a1ae33d61428f9ee
yanghaochang@xiaoniuhy.com <yanghaochang@xiaoniuhy.com>
2020-05-25 17:27:57 +0800

    1 已调通并部署测试完成

---
+++ datax-admin/src/main/java/com/wugui/datax/admin/tool/database/ColumnInfo.java
@@ -0,0 +1,63 @@
+package com.wugui.datax.admin.tool.database;
+
+import lombok.Data;
+
+/**
+ * 字段信息
+ *
+ * @author zhouhongfa@gz-yibo.com
+ * @version 1.0
+ * @since 2019/7/30
+ */
+@Data
+public class ColumnInfo {
+    /**
+     * 字段名称
+     */
+    private String name;
+    /**
+     * 注释
+     */
+    private String comment;
+    /**
+     * 字段类型
+     */
+    private String type;
+
+    /**
+     * 是否是主键列
+     */
+    private Boolean ifPrimaryKey;
+
+    public String getName () {
+        return name;
+    }
+
+    public void setName ( String name ) {
+        this.name = name;
+    }
+
+    public String getComment () {
+        return comment;
+    }
+
+    public void setComment ( String comment ) {
+        this.comment = comment;
+    }
+
+    public String getType () {
+        return type;
+    }
+
+    public void setType ( String type ) {
+        this.type = type;
+    }
+
+    public Boolean getIfPrimaryKey () {
+        return ifPrimaryKey;
+    }
+
+    public void setIfPrimaryKey ( Boolean ifPrimaryKey ) {
+        this.ifPrimaryKey = ifPrimaryKey;
+    }
+}
